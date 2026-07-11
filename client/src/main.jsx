de43b4a481797fff04e50d71061cb07aac594b2d
mitushgupta1507 <mitushgupta1507@gmail.com>
2026-07-12 01:33:25 +0530

    Fix folder casing for Vercel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { Toaster } from "react-hot-toast";
 
 import "./index.css";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./Context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
