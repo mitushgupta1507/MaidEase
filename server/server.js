e416f43723e4c8d8e7948e728432e25fb59c1b33
mitushgupta1507 <mitushgupta1507@gmail.com>
2026-07-12 23:59:03 +0530

    Temporarily allow all origins

--- server/server.js
+++ server/server.js
@@ -48,18 +48,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin(origin, callback) {
-      // Allow Postman and server-to-server requests
-      if (!origin) {
-        return callback(null, true);
-      }
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: true,
     credentials: true,
   })
 );
